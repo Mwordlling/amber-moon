af8fd8bbf8b4103839b51788d6a826ec12b0de86
yevheniia <evgesha.drozdova@gmail.com>
2018-03-26 12:51:51 +0300

    click_me mobile button

--- js/click_me.js
+++ js/click_me.js
@@ -2,3 +2,4 @@
  * Created by yevheniia on 26.03.18.
  */
 d3.select("#figure1").append('p').text("Click me").attr('class', "click_me");
+d3.select("#figureM1").append('p').text("Click me").attr('class', "click_me_M");
