47151d312da5a904b768ead0a1ed46ae003642b2
yevheniia <evgesha.drozdova@gmail.com>
2018-03-26 14:05:58 +0300

    click_me mobile button

--- js/click_me.js
+++ js/click_me.js
@@ -1,5 +1,12 @@
 /**
  * Created by yevheniia on 26.03.18.
  */
-d3.select("#figure1").append('p').text("Click me").attr('class', "click_me");
-d3.select("#figureM1").append('p').text("Click me").attr('class', "click_me_M");
+d3.select("#figure1")
+    .append('p')
+    .html("click me <i class='fa fa-angle-right'></i>")
+    .attr('class', "click_me");
+
+d3.select("#figureM1")
+    .append('p')
+    .html("click me <i class='fa fa-angle-right'></i>")
+    .attr('class', "click_me");
